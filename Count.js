cad83b044f48374cd4cb01360342062cc597996f
StuartUdall <117107436+StuartUdall@users.noreply.github.com>
2022-11-11 13:43:52 +0000

    Update Count.js

--- Count.js
+++ Count.js
@@ -10,8 +10,8 @@ define('modules/customScript', ["ui.api.v1", "models/server/callConstants", "mod
     // IMPORTANT: make sure that the call variable is on the layout tab of the campaign profile, and is NOT read-only
     const contactFieldsToMap = [
       {
-        contactField: 'School',
-        callVariable: 'Caller.School'
+        contactField: 'accountNum',
+        callVariable: 'Reporting.AccountNum'
       }
 
     ];
